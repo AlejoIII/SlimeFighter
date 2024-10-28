1c1a14c003b6f128a4e9d98c801cb688b84d747f
AlejoIII <Loloyum34@gmail.com>
2024-10-28 23:33:31 +0100

    Fight update

--- src/main/java/com/slimefighter/slimefighter/GameController.java
+++ src/main/java/com/slimefighter/slimefighter/GameController.java
@@ -38,7 +38,8 @@
         private Label boostedAttribute2;
         @FXML
         private Label ChoosenAttribute;
-
+        @FXML
+        private Button RestartButton;
 
         @FXML
         private Slime[] slimes = {
@@ -59,7 +60,6 @@
                 new Slime("/images/SlimeRancher/Slimes/Slime_Zorro.png", 70, 40, 90),
         };
 
-
         private enum Atributos {
             ATAQUE,
             DEFENSA,
@@ -73,88 +73,57 @@
             FightButton.setOnAction(e -> {
                 Fight();
             });
-
-            /*LuckButton.setOnAction(e -> {
-                Luck();
-            });*/
         }
 
-
-
-        /*private void Luck() {
-            // Seleccionar slime aleatorio
-            int randomSlime = (int) (Math.random() * slimes.length);
-            // Seleccionar atributo aleatorio
-            int randomAtri = (int) (Math.random() * 3);
-            // Seleccionar valores aleatorios
-            int randomVal = (int) (Math.random() * 100) + 1;
-
-
-
-
-            // Ajustar atributos
-            switch (Atributos.values()[randomAtri]) {
-                case ATAQUE:
-                    slimes[randomSlime].setAtaque(slimes[randomSlime].getAtaque() + randomVal);
-                    break;
-                case DEFENSA:
-                    slimes[randomSlime].setDefensa(slimes[randomSlime].getDefensa() + randomVal);
-                    break;
-                case VELOCIDAD:
-                    slimes[randomSlime].setVelocidad(slimes[randomSlime].getVelocidad() + randomVal);
-                    break;
-            }
-        }*/
-
         private void Fight() {
-            // Seleccionar atributo aleatorio
-            int randomAtri= (int) (Math.random() * 3);
-            // Mostrar  el atributo elegido para la pelea
-            ChoosenAttribute.setText(Atributos.values()[randomAtri].toString());
-            // mostrar la suma del atributo elegido para la pelea de cada slime cada vez que se presiona el boton de pelea
-            boostedAttribute1.setText("+" + (slimes[0].getAtaque() + slimes[0].getDefensa() + slimes[0].getVelocidad()));
-            boostedAttribute2.setText("+" + (slimes[1].getAtaque() + slimes[1].getDefensa() + slimes[1].getVelocidad()));
-
-
-            // Seleccionar valores aleatorios
-            int randomVal = (int) (Math.random() * 100) + 1;
+            // Seleccionamos el atributo aleatorio
+            int randomAtri = (int) (Math.random() * 3);
+            // Mostramos el atributo elegido para la pelea
+            ChoosenAttribute.setText("Atributo seleccionado: " + Atributos.values()[randomAtri].toString());
+            // Seleccionamos los valores aleatorios entre 1 y 100 para cada slime
+            int randomVal1 = (int) (Math.random() * 100) + 1;
             int randomVal2 = (int) (Math.random() * 100) + 1;
 
             boolean FirstWinPlayer;
-
-            // Comparar atributos
+            // Mostramos la suma del atributo elegido mas el valor aleatorio para cada slime
             switch (Atributos.values()[randomAtri]) {
                 case ATAQUE:
-                    FirstWinPlayer = (slimes[0].getAtaque() + randomVal) > (slimes[1].getAtaque() + randomVal2);
+                    boostedAttribute1.setText("Ataque: " + slimes[0].getAtaque() + " + " + randomVal1);
+                    boostedAttribute2.setText("Ataque: " + slimes[1].getAtaque() + " + " + randomVal2);
+                    FirstWinPlayer = (slimes[0].getAtaque() + randomVal1) > (slimes[1].getAtaque() + randomVal2);
                     break;
                 case DEFENSA:
-                    FirstWinPlayer = (slimes[0].getDefensa() + randomVal) > (slimes[1].getDefensa() + randomVal2);
+                    boostedAttribute1.setText("Defensa: " + slimes[0].getDefensa() + " + " + randomVal1);
+                    boostedAttribute2.setText("Defensa: " + slimes[1].getDefensa() + " + " + randomVal2);
+                    FirstWinPlayer = (slimes[0].getDefensa() + randomVal1) > (slimes[1].getDefensa() + randomVal2);
                     break;
                 case VELOCIDAD:
-                    FirstWinPlayer = (slimes[0].getVelocidad() + randomVal) > (slimes[1].getVelocidad() + randomVal2);
+                    boostedAttribute1.setText("Velocidad: " + slimes[0].getVelocidad() + " + " + randomVal1);
+                    boostedAttribute2.setText("Velocidad: " + slimes[1].getVelocidad() + " + " + randomVal2);
+                    FirstWinPlayer = (slimes[0].getVelocidad() + randomVal1) > (slimes[1].getVelocidad() + randomVal2);
                     break;
                 default:
                     return;
             }
-            // Manejar  vidas
+            // Manejamos la visibilidad de las vidas segun el resultado de la pelea
             LifeVisibility(FirstWinPlayer);
-
-            // si un jugador se queda sin vidas se reinicia el juego
-
-
         }
 
         private void LifeVisibility(boolean FirstWinPlayer) {
-            // Restar vida
+            // Restamos las vidas
             if (FirstWinPlayer) {
                 RestLife(SecondPlayerLife1, SecondPlayerLife2, SecondPlayerLife3);
             } else {
                 RestLife(FirstPlayerLife1, FirstPlayerLife2, FirstPlayerLife3);
             }
+
+            if (Endgame()) {
+                return;
+            }
         }
 
         private void RestLife(ImageView Life1, ImageView Life2, ImageView Life3) {
-            // Ocultar vida
+            // Ocultamos la vida
             if (Life1.isVisible()) {
                 Life1.setVisible(false);
             } else if (Life2.isVisible()) {
@@ -164,39 +133,68 @@
             }
         }
 
+        private boolean Endgame() {
+            if (!FirstPlayerLife1.isVisible() && !FirstPlayerLife2.isVisible() && !FirstPlayerLife3.isVisible()) {
+                EndgameMessage("¡El slime 1 ha ganado!");
+                return true;
+            } else if (!SecondPlayerLife1.isVisible() && !SecondPlayerLife2.isVisible() && !SecondPlayerLife3.isVisible()) {
+                EndgameMessage("¡El slime 2 ha ganado!");
+                return true;
+            }
+            return false;
+        }
+
+        private void EndgameMessage(String message) {
+            // Mostramos el mensaje de victoria
+            ChoosenAttribute.setText(message);
+            // Desactivamos los botones
+            FightButton.setDisable(true);
+            LuckButton.setDisable(true);
+        }
+
+        @FXML
+        private void RestartGame() {
+            // Volvemos a mostrar las vidas
+            FirstPlayerLife1.setVisible(true);
+            FirstPlayerLife2.setVisible(true);
+            FirstPlayerLife3.setVisible(true);
+            SecondPlayerLife1.setVisible(true);
+            SecondPlayerLife2.setVisible(true);
+            SecondPlayerLife3.setVisible(true);
+            // Activamos botones
+            FightButton.setDisable(false);
+            LuckButton.setDisable(false);
+            // Restablecemos los mensajes
+            ChoosenAttribute.setText("");
+            // Volvemos a elegir slimes aleatorios
+            setRandomSlimes();
+        }
+
         private void setRandomSlimes() {
-            // Seleccionar dos slimes aleatorios
+            // Seleccionamos dos slimes aleatorios
             int randomSlime1 = (int) (Math.random() * slimes.length + Atributos.values().length);
             // Los slimes no pueden ser iguales
             int randomSlime2;
             do {
                 randomSlime2 = (int) (Math.random() * slimes.length+ Atributos.values().length);
             } while (randomSlime1 == randomSlime2);
-
-            // Mostrar atributos
+            // Mostramos los atributos
             showAttribute(slimes[randomSlime1]);
             showAttribute1(slimes[randomSlime2]);
-
-
-            // Mostrar slimes
+            // Mostramos los  slimes
             FirstPlayer.setImage(slimes[randomSlime1].getImage());
             SecondPlayer.setImage(slimes[randomSlime2].getImage());
-
-
-            // Ajustar tamaño
+            // Ajustamos el tamaño
             FirstPlayer.setFitHeight(200);
             FirstPlayer.setFitWidth(200);
             SecondPlayer.setFitHeight(200);
             SecondPlayer.setFitWidth(200);
         }
-
-        //añadir los atributos de los slimes escogidos
+        //añadimos los atributos de los slimes escogidos
         private void showAttribute(Slime slime){
             showAttribute.setText("Ataque: " + slime.getAtaque() + "\n" + "Defensa: " + slime.getDefensa() + "\n" + "Velocidad: " + slime.getVelocidad());
         }
-
         private void showAttribute1(Slime slime) {
             showAttribute1.setText("Ataque: " + slime.getAtaque() + "\n" + "Defensa: " + slime.getDefensa() + "\n" + "Velocidad: " + slime.getVelocidad());
         }
-
     }
